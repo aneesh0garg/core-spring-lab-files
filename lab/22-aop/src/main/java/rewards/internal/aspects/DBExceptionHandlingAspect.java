087a2de534136482a6f998999b16011e0f58100b
Aneesh Garg <aneeshgarg20@gmail.com>
2025-12-20 19:15:12 +0530

    Introducing AOP lab

--- lab/22-aop/src/main/java/rewards/internal/aspects/DBExceptionHandlingAspect.java
+++ lab/22-aop/src/main/java/rewards/internal/aspects/DBExceptionHandlingAspect.java
@@ -1,13 +1,16 @@
 package rewards.internal.aspects;
 
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.stereotype.Component;
 import rewards.internal.exception.RewardDataAccessException;
 
 
-@Aspect	
+@Aspect
+@Component
 public class DBExceptionHandlingAspect {
 	
 	public static final String EMAIL_FAILURE_MSG = "Failed sending an email to Mister Smith : ";
@@ -22,7 +25,8 @@ public class DBExceptionHandlingAspect {
 	//	  exceptions thrown by Repository class methods.
 	//	- Select the advice type that seems most appropriate.
 	
-	public void implExceptionHandling(RewardDataAccessException e) {
+	@AfterThrowing(value = "execution(public * rewards.internal.*.*Repository.*(..))", throwing = "e")
+    public void implExceptionHandling(RewardDataAccessException e) {
 		// Log a failure warning
 		logger.warn(EMAIL_FAILURE_MSG + e + "\n");
 	}
